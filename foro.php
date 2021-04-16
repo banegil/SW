2dffe43d9496fe821e2432921075ad39d43f381e
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-04-16 14:25:58 +0200

    Create foro.php

---
+++ foro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset= "utf-8">
+		<title>Foro</title>
+	</head>
+	
+	<body>
+
+	<?php
+		require("cabecera.php");
+	?>
+
+	<main>
+		<article>
+			<h1>Foro</h1>
+      <p> EN CONSTRUCCIÓN </p>
+		</article>
+	</main>
+	
+	<?php
+		require("pie.php");
+	?>
+	</div>
+
+	</body>
+</html>
