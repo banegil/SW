2bfbf6f8f6f514f275c53d33ef6396a0a87e0ffc
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-04-16 14:25:11 +0200

    Create colabora.php

---
+++ colabora.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset= "utf-8">
+		<title>¡Colabora con nosotros!</title>
+	</head>
+	
+	<body>
+
+	<?php
+		require("cabecera.php");
+	?>
+
+	<main>
+		<article>
+			<h1>Cómo colaborar con nosotros: </h1>
+      <p> EN CONSTRUCCIÓN </p>
+		</article>
+	</main>
+	
+	<?php
+		require("pie.php");
+	?>
+	</div>
+
+	</body>
+</html>
