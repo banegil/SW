d2706c57ead75bb836f6490740059756cc7f7c73
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-04-16 14:26:31 +0200

    Update pie.php

--- pie.php
+++ pie.php
@@ -3,8 +3,8 @@
 		<li> <a href= > Inicio </li>
 		<li> <a href= > Animales en adopción </li>
 		<li> <a href= > Protectoras </li>
-		<li> <a href= > Historias Felices </li>
-		<li> <a href= > Colabora con nosotros </li>
-		<li> <a href= > Foro </li>
+		<li> <a href="historiasFelices.php" > Historias Felices </li>
+		<li> <a href="colabora.php" > Colabora con nosotros </li>
+		<li> <a href="foro.php" > Foro </li>
 	</ul>
 </footer>
