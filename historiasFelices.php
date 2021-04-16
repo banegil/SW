e7a59c8d781b947552764d874f83fe25a096f543
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-04-16 14:23:54 +0200

    Create historiasFelices.php

---
+++ historiasFelices.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset= "utf-8">
+		<title>Historias Felices</title>
+	</head>
+	
+	<body>
+
+	<?php
+		require("cabecera.php");
+	?>
+
+	<main>
+		<article>
+			<h1>Historias Felices</h1>
+      <p> EN CONSTRUCCIÓN </p>
+		</article>
+	</main>
+	
+	<?php
+		require("pie.php");
+	?>
+	</div>
+
+	</body>
+</html>
