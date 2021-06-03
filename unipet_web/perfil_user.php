916b8f33b27ccecc55aa082b2d354d78857ee020
rcincea <49287636+rcincea@users.noreply.github.com>
2021-06-03 22:39:50 +0200

    Update perfil_user.php
    Cambio para usar CSS y plantilla

--- unipet_web/perfil_user.php
+++ unipet_web/perfil_user.php
@@ -1,78 +1,71 @@
-<!DOCTYPE html>
-<html>
-<header>
-<!-- Aqui va el script del menu de cabecera-->
-<meta charset= "utf-8">
-<title>Perfil Usuario</title>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
-</header> 
 
-<body>
-    
-<div id="contenedor">
 <?php
 require_once __DIR__."/includes/Usuario.php";
 require_once __DIR__."/includes/UsuarioDB.php";
 require_once __DIR__."/includes/AnimalDB.php";
 
-require("cabecera.php");
-?>
-	<main>
-		<article>
-<?php
+$tituloPagina = 'PerfilUsuario';
+$contenidoPrincipal = '<link rel="stylesheet" type="text/css" href="perfil.css" />';
+
+$contenidoPrincipal.= "<div id='perfilcontenedor'>";
 if(estaLogado()){
     $idUser = idUsuarioLogado();
     if($idUser != false){ // comprobamos si hemos recibido un id de usuario
         $usuario = USUARIO::buscaPorDNI($idUser); // funcion estática para buscar en la base de datos al usuario por el dni
-        //echo '<img src="data:imagenes;base64,' . base64_encode($usuario->getImagen()) . ' "width=15%";>';
         
         
         if($usuario !=false){ // comprobamos si el usuario esta en la base de datos
-            echo "<p> DNI: ".$usuario->getDni(). "</p>";
-            echo "<p> NOMBRE: ".$usuario->getNombre(). "</p>";
-            echo "<p> APELLIDO: ".$usuario->getApellido(). "</p>";
-            echo "<p> TELEFONO: ".$usuario->getTelefono(). "</p>";
-            echo "<p> EMAIL: ".$usuario->getEmail(). "</p>";
-            echo "<p> FECHA NACIMIENTO : ".$usuario->getNacimiento(). "</p>";
-            echo "<p> DIRECCION: ".$usuario->getDireccion(). "</p>";
-            echo "<p> TIPO: ".$usuario->getTipo(). "</p>";
+            $contenidoPrincipal .="<p> DNI: ".$usuario->getDni(). "</p>";
+            // aqui iria el div para foto nombre y apellido
+            $contenidoPrincipal .= "<div id='contenedorNombre'>";
+            $contenidoPrincipal .= "<div id='contenedorNameApellido'>";
+            $contenidoPrincipal .="<p> NOMBRE: ".$usuario->getNombre(). "</p>";
+            $contenidoPrincipal .="<p> APELLIDO: ".$usuario->getApellido(). "</p>";
+            $contenidoPrincipal .= "</div>";
+            $contenidoPrincipal .= "</div>";
+            // contenedor con el resto de datos
+            $contenidoPrincipal .= "<div id='contenedorDatos'>";
+            $contenidoPrincipal .= "<div id='contenedorDatosIzquierda'>";
+            $contenidoPrincipal .= "<p> TELEFONO: ".$usuario->getTelefono(). "</p>";
+            $contenidoPrincipal .= "<p> EMAIL: ".$usuario->getEmail(). "</p>";
+            $contenidoPrincipal .= "<p> FECHA NACIMIENTO : ".$usuario->getNacimiento(). "</p>";
+            $contenidoPrincipal .= "<p> DIRECCION: ".$usuario->getDireccion(). "</p>";
+            $contenidoPrincipal .= "<p> TIPO: ".$usuario->getTipo(). "</p>";
+            $contenidoPrincipal .= "</div>";
             //animales adoptados y apadrinados
+            $contenidoPrincipal .= "<div id='contenedorDatosDerecha'>";
             $adoptado = Animal::getAdoptados($usuario->getDni());
-            echo "<p> ANIMALES ADOPTADOS:" ."</p>";
+            $contenidoPrincipal .= "<p> ANIMALES ADOPTADOS:" ."</p>";
             if($adoptado != false){
                 foreach($adoptado as $i){ 
-                echo "<p>" .$i->getNombre()."</p>";
+                    $contenidoPrincipal .= "<p>" .$i->getNombre()."</p>";
                 }
-            } else echo "<p> Ninguno </p>";			
+            } else $contenidoPrincipal .= "<p> Ninguno </p>";			
             $apadrinado = Animal::getApadrinados($usuario->getDni());
-            echo "<p> ANIMALES APADRINADOS:" ."</p>";
+            $contenidoPrincipal .="<p> ANIMALES APADRINADOS:" ."</p>";
             if($apadrinado != false){
                 foreach($apadrinado as $i){ 
-                echo "<p>" .$i->getNombre()."</p>";
+                    $contenidoPrincipal .= "<p>" .$i->getNombre()."</p>";
                 }
-            } else echo "<p> Ninguno </p>";
+            } else $contenidoPrincipal .= "<p> Ninguno </p>";
+                 $contenidoPrincipal .= "</div>";
+            $contenidoPrincipal .= "</div>";
         }
         else{
-            echo "<h2>El usuario no existe</h2>";
+            $contenidoPrincipal .= "<h2>El usuario no existe</h2>";
         }
-        ?>
-        <form method="GET" action="editarPerfilUsuario.php">
-            <input type="submit" name="action" value="Editar Perfil"/>
-            <?php
-        
+        $contenidoPrincipal .= "<div id='botonesContenedor'>";
+        $contenidoPrincipal .= <<<EOS
+        <a class='boton' href=editarPerfilUsuario.php> Editar perfil</a>
+        EOS;
+        $contenidoPrincipal .= "</div>";
     }
     else{
-        echo "<h2>usuario no válido</h2>";
+        $contenidoPrincipal .="<h2>usuario no válido</h2>";
     }
 }
 else{
-	echo "<h1> ¡NO SE HA INICIADO SESIÓN! </h1>";
-}?>			</article>
-		</main>
-	</div>
-<?php
-require("pie.php");
-    ?>
- 
-</body>
-</html>
+	$contenidoPrincipal .="<h1> ¡NO SE HA INICIADO SESIÓN! </h1>";
+}
+$contenidoPrincipal .= "</div>";
+ require __DIR__ . '/includes/plantillas/plantilla.php';
