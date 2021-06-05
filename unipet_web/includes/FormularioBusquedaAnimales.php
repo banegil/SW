8952fd332c072df8855090049d0fd9b40ff23bac
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-06 00:13:47 +0200

    Update FormularioBusquedaAnimales.php

--- unipet_web/includes/FormularioBusquedaAnimales.php
+++ unipet_web/includes/FormularioBusquedaAnimales.php
@@ -1,6 +1,5 @@
 <?php
 require 'Form.php';
-require_once 'AnimalDB.php';
 require_once 'comun/listaAnimales.php';
 class FormularioBusquedaAnimales extends Form
 {
@@ -24,7 +23,7 @@ class FormularioBusquedaAnimales extends Form
 			            <div ><button type="submit" name="buscar">BUSCAR</button></div>
       </fieldset>
 EOF;
-	if (!$this->enviado) $camposFormulario .= listaAnimales(Animal::buscaAnimales(null,"Todos"));
+	if (!$this->enviado) $camposFormulario .= listaAnimales(es\ucm\fdi\aw\Animal::buscaAnimales(null,"Todos"));
       return $camposFormulario;
   }
 
@@ -36,7 +35,7 @@ EOF;
     $nombreAnimal = isset($_POST['nombreAnimal']) ? $_POST['nombreAnimal'] : null;
 	$tipoAnimal = isset($_POST['tipoAnimal']) ? $_POST['tipoAnimal'] : "Todos";
 	
-	$busqueda = Animal::buscaAnimales($nombreAnimal,$tipoAnimal);
+	$busqueda = es\ucm\fdi\aw\Animal::buscaAnimales($nombreAnimal,$tipoAnimal);
 	if ($busqueda != null) {
 		$result['busqueda'] = listaAnimales($busqueda);
 		$this->enviado = true;
