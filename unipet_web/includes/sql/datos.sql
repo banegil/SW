009f2ee4c35b09b96d9092e87d580c39d6e09410
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-05 15:01:29 +0200

    Actualizacion BD

--- unipet_web/includes/sql/datos.sql
+++ unipet_web/includes/sql/datos.sql
@@ -25,51 +25,52 @@ SET time_zone = "+00:00";
 -- Volcado de datos para la tabla `animales`
 --
 
-INSERT INTO `animales` (`ID`, `nombre`, `nacimiento`, `tipo`, `raza`, `sexo`, `peso`, `ingreso`, `protectora`, `historia`, `DNI`, `urgente`) VALUES
-(1, 'Winkie', '2015-09-06', 'perro', 'Teckel', 'hembra', '4.2', '2021-04-15', 1, 'Mucho texto', '54494722V', 0),
+INSERT INTO `animales` (`ID`, `nombre`, `nacimiento`, `tipo`, `raza`, `sexo`, `peso`, `ingreso`, `protectora`, `historia`, `ID_usuario`, `urgente`) VALUES
+(1, 'Winkie', '2015-09-06', 'perro', 'Teckel', 'hembra', '4.2', '2021-04-15', 1, 'Mucho texto', 7, 0),
 (2, 'Coco', '2021-04-15', 'gato', NULL, 'hembra', '3.0', '2021-04-15', 1, NULL, NULL, 1),
-(3, 'Thor', '2021-04-15', 'perro', 'Labrador', 'macho', '32.0', '2021-04-15', 1, 'TEXTO', '00000002A', 0),
+(3, 'Thor', '2021-04-15', 'perro', 'Labrador', 'macho', '32.0', '2021-04-15', 1, 'TEXTO', 4, 0),
 (4, 'Max', '2021-04-15', 'gato', NULL, 'macho', NULL, '2021-04-15', 2, NULL, NULL, 0),
 (5, 'Rocky', '2021-04-15', 'gato', NULL, 'macho', NULL, '2021-04-15', 1, NULL, NULL, 0),
 (6, 'Acua', '2021-04-15', 'gato', NULL, 'hembra', NULL, '2021-04-15', 2, NULL, NULL, 0),
+(7, 'Hannah', NULL, 'perro', NULL, 'hembra', NULL, '2021-04-16', 2, NULL, NULL, 0);
 (8, 'Bianca', '2021-04-15', 'perro', 'Samoyedo', 'hembra', NULL, '2021-04-15', 3, NULL, NULL, 1),
 (9, 'Leo', '2021-04-15', 'gato', NULL, 'macho', NULL, '2021-04-15', 1, NULL, NULL, 0),
-(10, 'Ava', '2021-04-15', 'gato', 'Scottish Fold', 'hembra', '2.9', '2021-04-15', 2, 'En el momento que Rares entró a la protectora, él y Ava se miraron y su conexión fue instantáneo, forjando una unión que nada los separará.', '00000003A', 0),
+(10, 'Ava', '2021-04-15', 'gato', 'Scottish Fold', 'hembra', '2.9', '2021-04-15', 2, 'En el momento que Rares entró a la protectora, él y Ava se miraron y su conexión fue instantáneo, forjando una unión que nada los separará.', 4, 0),
 (11, 'Simba', '2021-04-15', 'perro', NULL, 'macho', NULL, '2021-04-15', 3, NULL, NULL, 0),
 (12, 'Cloe', '2021-04-15', 'perro', NULL, 'hembra', NULL, '2021-04-15', 1, NULL, NULL, 0),
 (13, 'Lucas', '2021-04-15', 'gato', NULL, 'macho', NULL, '2021-04-15', 1, NULL, NULL, 0),
-(14, 'Frida', '2021-04-15', 'gato', 'Persa', 'hembra', '3.8', '2021-04-15', 1, 'Desde que conoció a Enrique, se volvió el gato más feliz de toda España.', '00000000A', 0),
+(14, 'Frida', '2021-04-15', 'gato', 'Persa', 'hembra', '3.8', '2021-04-15', 1, 'Desde que conoció a Enrique, se volvió el gato más feliz de toda España.', 1, 0),
 (15, 'Zeus', '2021-04-15', 'perro', NULL, 'macho', NULL, '2021-04-15', 3, NULL, NULL, 0),
 (16, 'Iris', '2021-04-15', 'perro', NULL, 'hembra', NULL, '2021-04-15', 2, NULL, NULL, 1),
-(17, 'Hannah', NULL, 'perro', NULL, 'hembra', NULL, '2021-04-16', 2, NULL, NULL, 0);
+
 
 --
 -- Volcado de datos para la tabla `apadrinados`
 --
 
-INSERT INTO `apadrinados` (`ID`, `DNI`, `cantidad`) VALUES
-(7, '51739423A', '5.00');
+INSERT INTO `apadrinados` (`ID`, `ID_usuario`, `cantidad`) VALUES
+(7, 4, '5.00');
 
 --
 -- Volcado de datos para la tabla `contrato_adopcion`
 --
 
-INSERT INTO `contrato_adopcion` (`DNI`, `ID`, `formulario`, `estado`, `fecha`) VALUES
-('54494722V', 1, 'Mucho texto', 'Aprobado', '2021-04-15'),
-('54494722V', 12, 'Mucho texto', 'Rechazado', '2021-04-15'),
-('54494722V', 4, 'Mucho texto', 'EnTramite', '2021-04-15'),
-('51739423A', 8, 'Mucho texto', 'PendCita', '2021-04-15'),
-('00000000A', 14, 'Texto', 'Aprobado', '2021-04-15'),
-('00000002A', 3, 'TEXTO', 'Aprobado', '2021-04-15'),
-('00000003A', 10, 'TEXTO', 'Aprobado', '2021-04-15'),
-('00000000A', 15, '¿Has tenido (o tienes) animales? si \r\n								¿En que tipo de domicilio vives diariamente? ninguno \r\n								¿Viven niños en el mismo domicilio en el que se encontrará el animal? no \r\n								¿Cuantas horas estaría solo el animal? 24\r\n								¿Cual es tu motivación por adoptar un animal? tengo hambre', 'EnTramite', '2021-04-16');
+INSERT INTO `contrato_adopcion` (`ID_usuario`, `ID`, `formulario`, `estado`, `fecha`) VALUES
+(7, 1, 'Mucho texto', 'Aprobado', '2021-04-15'),
+(7, 12, 'Mucho texto', 'Rechazado', '2021-04-15'),
+(3, 4, 'Mucho texto', 'EnTramite', '2021-04-15'),
+(3, 8, 'Mucho texto', 'PendCita', '2021-04-15'),
+(2, 14, 'Texto', 'Aprobado', '2021-04-15'),
+(2, 3, 'TEXTO', 'Aprobado', '2021-04-15'),
+(1, 10, 'TEXTO', 'Aprobado', '2021-04-15'),
+(1, 15, '¿Has tenido (o tienes) animales? si \r\n								¿En que tipo de domicilio vives diariamente? ninguno \r\n								¿Viven niños en el mismo domicilio en el que se encontrará el animal? no \r\n								¿Cuantas horas estaría solo el animal? 24\r\n								¿Cual es tu motivación por adoptar un animal? tengo hambre', 'EnTramite', '2021-04-16');
 
 --
 -- Volcado de datos para la tabla `entradas`
 --
 
-INSERT INTO `entradas` (`DNI`, `numero`, `hilo`, `comentario`, `fecha`) VALUES
-('54494722V', 1, 1, 'Hola!\r\nPara poder adoptar a un animal debes de ir a la publicación de alguno de ellos y darle al botón de \"Adoptar\", siempre y cuando estés registrado claro. Posteriormente debes de rellenar el formulario para iniciar el proceso de adopción y cuando lo envíes algún chico/a de la protectora te avisará para proseguir.\r\n', '2021-04-16');
+INSERT INTO `entradas` (`ID_usuario`, `numero`, `hilo`, `comentario`, `fecha`) VALUES
+(3, 1, 1, 'Hola!\r\nPara poder adoptar a un animal debes de ir a la publicación de alguno de ellos y darle al botón de \"Adoptar\", siempre y cuando estés registrado claro. Posteriormente debes de rellenar el formulario para iniciar el proceso de adopción y cuando lo envíes algún chico/a de la protectora te avisará para proseguir.\r\n', '2021-04-16');
 
 --
 -- Volcado de datos para la tabla `fichas`
@@ -82,8 +83,8 @@ INSERT INTO `fichas` (`ID`, `vacunas`, `observaciones`) VALUES
 -- Volcado de datos para la tabla `hilos`
 --
 
-INSERT INTO `hilos` (`NUMERO`, `titulo`, `fecha`, `DNI`, `comentario`) VALUES
-(1, '¿Cómo se adopta?', '2021-04-15', '51739423A', 'Hola!, soy nuevo aquí y no sé cómo se adopta, alguien me echa una mano?');
+INSERT INTO `hilos` (`NUMERO`, `titulo`, `fecha`, `ID_usuario`, `comentario`) VALUES
+(1, '¿Cómo se adopta?', '2021-04-15', 7, 'Hola!, soy nuevo aquí y no sé cómo se adopta, alguien me echa una mano?');
 
 --
 -- Volcado de datos para la tabla `protectoras`
