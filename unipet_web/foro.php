5f410ab0883c7a9a6b2312b2a0271fa4ae5aea21
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-06 20:48:25 +0200

    Update foro.php

--- unipet_web/foro.php
+++ unipet_web/foro.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
-require_once __DIR__.'/includes/ForoDB.php';
 require_once __DIR__.'/includes/comun/listaHilos.php';
 
 $tituloPagina = 'Foro';
-$hilos = listaHilosPaginado(Hilo::getHilos(),5,1);
+$hilos = listaHilosPaginado(es\ucm\fdi\aw\Hilo::getHilos(),5,1);
 
 $contenidoPrincipal = <<<EOS
 $hilos
