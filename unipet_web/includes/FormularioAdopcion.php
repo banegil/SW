0046ff681c402299d262ee9a218cad8686019e3a
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-07 11:48:54 +0200

    Update FormularioAdopcion.php

--- unipet_web/includes/FormularioAdopcion.php
+++ unipet_web/includes/FormularioAdopcion.php
@@ -23,7 +23,11 @@ EOS;
 					<input type="radio" name="animalAnt" value="no" checked/> No
 				</div>
 				
-				<div><label>¿En que tipo de domicilio vives diariamente?</label> 
+				<div><label>¿Quien se va a encargar de la mascota?</label> 
+					<input type="text" name="seEncargaMascota" value="" size="50"/>
+				</div>
+				
+				<div><label>¿En que tipo de domicilio vivirá diariamente?</label> 
 					<select name="typeOfHouse">
 					  <option value="apartamento" selected>Apartamento</option>
 					  <option value="pisoGrande">Apartamento grande</option>
@@ -36,6 +40,11 @@ EOS;
 				<div><label>¿Viven niños en el mismo domicilio en el que se encontrará el animal?</label> 
 					<input type="radio" name="niños" value="si" /> Si
 					<input type="radio" name="niños" value="no" checked/> No
+				</div>
+				
+				<div><label>¿Todos los que vivan en la casa están de acuerdo con la adopción?</label> 
+					<input type="radio" name="deAcuerdo" value="si" /> Si
+					<input type="radio" name="deAcuerdo" value="no" checked/> No
 				</div>
 				
 				<div><label>¿Cuantas horas estaría solo el animal?</label> 
@@ -57,10 +66,14 @@ EOS;
 	    	$errores = array();
 			
 	    	$animalAnt = isset($_POST['animalAnt']) ? $_POST['animalAnt'] : null;
+			
+			$seEncargaMascota = isset($_POST['seEncargaMascota']) ? $_POST['seEncargaMascota'] : null;
 
 			$typeOfHouse = isset($_POST['typeOfHouse']) ? $_POST['typeOfHouse'] : null;
 			
 			$niños = isset($_POST['niños']) ? $_POST['niños'] : null;
+			
+			$deAcuerdo = isset($_POST['deAcuerdo']) ? $_POST['deAcuerdo'] : null;
 			
 			$horaSolo = isset($_POST['horaSolo']) ? $_POST['horaSolo'] : null;
 			
@@ -72,6 +85,29 @@ EOS;
 			}
 			else{
 				
+				$respuesta1 = $_POST['animalAnt'];
+				$respuesta2 = $_POST['seEncargaMascota'];
+				$respuesta3 = $_POST['typeOfHouse'];
+				$respuesta4 = $_POST['niños'];
+				$respuesta5 = $_POST['deAcuerdo'];
+				$respuesta6 = $_POST['horaSolo'];
+				$respuesta7 = $_POST['razonAdopcion'];
+
+				$textoFormulario = sprintf("¿Has tenido (o tienes) animales? %s 
+											¿Quien se va a encargar de la mascota? %s 
+											¿En que tipo de domicilio vivirá diariamente? %s 
+											¿Viven niños en el mismo domicilio en el que se encontrará el animal? %s 
+											¿Todos los que vivan en la casa están de acuerdo con la adopción? %s 
+											¿Cuantas horas estaría solo el animal? %s
+											¿Cual es tu motivación por adoptar un animal? %s"
+									  , $respuesta1
+									  , $respuesta2
+									  , $respuesta3
+									  , $respuesta4
+									  , $respuesta5
+									  , $respuesta6
+									  , $respuesta7);
+
 				$contract = Contrato::buscaPorDNIeID($_SESSION['id'],$_SESSION['idAnimal']);
 				
 				if (!$contract) {
