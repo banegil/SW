377ac04928a1cd3bbd5f323b3e70a23824e41276
auroradaza <64855755+auroradaza@users.noreply.github.com>
2021-06-08 00:46:13 +0200

    Add files via upload

---
+++ unipet_web/verlistausuarios.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/comun/listaUsuarios.php';
+
+$usuarios = listaUsuarios(es\ucm\fdi\aw\Usuario:: getUsuarios());
+
+$tituloPagina = 'Lista de usuarios';
+
+$contenidoPrincipal = <<<EOS
+	<h1>Usuarios</h1>
+	$usuarios
+	
+EOS;
+
+require __DIR__.'/includes/plantillas/plantilla.php';
