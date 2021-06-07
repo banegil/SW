cf03b7e608c69f3f5eaa5cd213205f4fdf65cee3
auroradaza <64855755+auroradaza@users.noreply.github.com>
2021-06-08 00:45:27 +0200

    Add files via upload

--- unipet_web/modificaProtectora.php
+++ unipet_web/modificaProtectora.php
@@ -1,44 +1,15 @@
 <?php
 
-require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'\includes\config.php';
 
-$tituloPagina = 'EditarProtectora';
-$protectora = es\ucm\fdi\aw\Protectora::buscaProtectoraPorId($id));
+$form = new es\ucm\fdi\aw\FormularioEditaProtectora();
+$htmlFormRegistro = $form->gestiona();
 
-if()
+$tituloPagina = 'Actualizar';
+
 $contenidoPrincipal = <<<EOS
-<h1>Editar Datos: </h1>
-<form id="formModificaProtectora" action="procesarAddProtectora.php" method="POST">
-<fieldset>
-	<legend>Rellene los datos de la protectora</legend>
-	
-	<div><label>ID:</label> 
-		<input type="number" name="idProtectora" value="<?=$id?>" size="25"/>
-	</div>
-	<?php                
-		$protectora = es\ucm\fdi\aw\Protectora::buscaProtectoraPorId($id));   
-	?>
-
-	
-	<div><label>Número de indentificación:</label> 
-		<input type="number" name="idProtectora" value="<?=$protectora->getId()?>" size="25"/>
-	</div>	
-	
-	<div><label>Nombre:</label> 
-		<input type="text" name="nameProtectora" value="<?=$protectora->getNombre()?>" size="25"/>
-	</div>
-	
-	<div><label>Direccion:</label> 
-		<input type="text" name="raza" value="<?=$protectora->getDireccion()?>" size="25"/>
-	</div>
-	
-	<div><label>Teléfono:</label> 
-		<input type="text" name="protectora" value="<?=$protectora->getTelefono()?>" size="25"/>
-	</div>
-
-	<div><button type="submit">Actualizar</button> <button type="reset">Borrar todo</button></div>
-</fieldset>
-</form>
+<h1>Actualizar protectora</h1>
+$htmlFormRegistro
 EOS;
 
-require __DIR__.'/includes/plantillas/plantilla.php';
+require_once __DIR__.'/includes/plantillas/plantilla.php';
