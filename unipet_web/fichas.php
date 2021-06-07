39d0518eb03624eeffa905fbe1d80e8d7aa68b15
rcincea <49287636+rcincea@users.noreply.github.com>
2021-06-07 22:05:35 +0200

    listaFichas

---
+++ unipet_web/fichas.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__.'/includes/config.php';
+require_once("includes/Ficha.php");
+require_once __DIR__.'/includes/comun/listaFichas.php';
+
+$tituloPagina = 'Lista Fichas';
+$fichas = es\ucm\fdi\aw\Ficha::getFichas();
+$muestraFichas = listaFichas($fichas);
+
+
+
+$contenidoPrincipal = <<<EOS
+$muestraFichas
+EOS;
+
+require __DIR__.'/includes/plantillas/plantilla.php';
