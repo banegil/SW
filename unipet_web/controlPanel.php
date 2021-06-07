a8e37ab880b9c3bf237c39af958197575e0f16bb
auroradaza <64855755+auroradaza@users.noreply.github.com>
2021-06-08 00:46:43 +0200

    Add files via upload

--- unipet_web/controlPanel.php
+++ unipet_web/controlPanel.php
@@ -6,30 +6,37 @@ require_once __DIR__.'/includes/comun/listaSolicitudes.php';
 
 $solicitudes = listaSolicitudes(es\ucm\fdi\aw\Contrato:: getSolicitudes());
 
-$tituloPagina = 'Colabora';
+$tituloPagina = 'Panel de control';
 
 $contenidoPrincipal = <<<EOS
-<h1>Solicitudes</h1>
-$solicitudes
-<h1>Protectoras</h1>
-<form action="protectoras.php">
-	<input type="submit" value="Ver lista" />
-</form>
+	<h1>Solicitudes</h1>
+	$solicitudes
+	
+	<h1>Protectoras</h1>
+	<form action="protectoras.php">
+		<input type="submit" value="Ver lista" />
+	</form>
 
-<form action="addProtectora.php">
-	<input type="submit" value="Añadir protectora" />
-</form>
+	<form action="addProtectora.php">
+		<input type="submit" value="Añadir protectora" />
+	</form>
 
-<h1>Animales</h1>
-</article>
-<form action="animalesAdopcion.php">
-	<input type="submit" value="Ver Lista" />
-</form>
+	<h1>Animales</h1>
+	</article>
+	<form action="animalesAdopcion.php">
+		<input type="submit" value="Ver Lista" />
+	</form>
 
-</article>
-<form action="addAnimal.php">
-	<input type="submit" value="Añadir animal" />
-</form>
+	</article>
+	<form action="addAnimal.php">
+		<input type="submit" value="Añadir animal" />
+	</form>
+	
+	<h1>Usuarios</h1>
+	</article>
+	<form action="verlistausuarios.php">
+		<input type="submit" value="Ver Lista" />
+	</form>
 EOS;
 
 require __DIR__.'/includes/plantillas/plantilla.php';
