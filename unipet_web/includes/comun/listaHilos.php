59f897ad2adea2ced0a24bd7291fe956daf4067e
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-08 02:12:33 +0200

    Add files via upload

--- unipet_web/includes/comun/listaHilos.php
+++ unipet_web/includes/comun/listaHilos.php
@@ -1,57 +1,4 @@
 <?php
-function listaHilosPaginado($hilos,$numPorPagina,$numPagina){
-	if ($hilos == null) {
-		$html = "<p> No se han encontrado hilos </p>";
-		return $html;
-	}	
-    $html = '<ul>';
-    $numHilos = count($hilos);
-    $haySiguientePagina = false;
-    if ($numHilos > $numPorPagina) {
-        $numHilos = $numPorPagina;
-        $haySiguientePagina = true;
-    }
-    $idx = 0;
-    while($idx < $numHilos) {
-        $hilo = $hilos[$idx];
-		$titulo = $hilo->getTitulo();
-        $href = "hiloForo.php?hilo=".$hilo->getID()."";
-        $html .= '<li>';
-        $html .= "<a href=\"$href\">$titulo</a>";
-        $html .= '</li>';
-        $idx++;
-    }
-    $html .= '</ul>';
-
-    // Controles de paginacion
-    $clasesPrevia='disabled';
-    $clasesSiguiente = 'disabled';
-    $hrefPrevia = '';
-    $hrefSiguiente = '';
-
-    if ($numPagina > 1) {
-        // Seguro que hay mensajes anteriores
-        $paginaPrevia = $numPagina - 1;
-        $clasesPrevia = '';
-        $hrefPrevia = 'href="' . RUTA_APP.'/mensajes.php?id='.$idMensajePadre . '&numPagina='. $paginaPrevia . '"&numPorPagina='. $numPorPagina . '"';
-    }
-
-    if ($haySiguientePagina) {
-        // Puede que haya mensajes posteriores
-        $paginaSiguiente = $numPagina + 1;
-        $clasesSiguiente = '';
-        $hrefSiguiente = 'href="' . RUTA_APP.'/mensajes.php?id='.$idMensajePadre . '&numPagina='. $paginaSiguiente . '&numPorPagina='. $numPorPagina . '"';
-    }
-
-    $html .=<<<EOS
-        <div>
-            Página: $numPagina, <a class="boton $clasesPrevia" $hrefPrevia>Previa</a><a class="boton $clasesSiguiente" $hrefSiguiente>Siguiente</a>
-        </div>
-EOS;
-
-    return $html;
-	
-}
 
 	function listaHilosMuestra($hilos, $muestra){
 		$html = '';
@@ -63,6 +10,25 @@ EOS;
 				$cantidad = count($hilos);
 			else
 				$cantidad = $muestra;
+			
+			$html.= '<hr />';
+			for($i = 0; $i < $cantidad; $i++){ 
+				$html.= '<h3>- <a href = "hiloForo.php?hilo='.$hilos[$i]->getID().'">'.$hilos[$i]->getTitulo().'</a></h3>';
+				$html.= '<p>'.$hilos[$i]->getComentario().'</p>';
+				$html.= '<hr />';
+			}
+		}
+		return $html;
+	}
+	
+		function listaHilos($hilos){
+		$html = '';
+		if($hilos == false){
+			$html.= '<p>No hay ningún hilo actualmente</p>';
+		}
+		else{
+			
+			$cantidad = count($hilos);
 			
 			$html.= '<hr />';
 			for($i = 0; $i < $cantidad; $i++){ 
