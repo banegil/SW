eedcc5f8e6f53554acee710081628f8b3dbe5079
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-08 02:11:16 +0200

    Add files via upload

--- unipet_web/foro.php
+++ unipet_web/foro.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/comun/listaHilos.php';
 
 $tituloPagina = 'Foro';
-$hilos = listaHilosPaginado(es\ucm\fdi\aw\Hilo::getHilos(),5,1);
+$hilos = listaHilos(es\ucm\fdi\aw\Hilo::getHilos());
 
 $contenidoPrincipal = <<<EOS
 $hilos
--- unipet_web/hiloForo.php
+++ unipet_web/hiloForo.php
@@ -1,20 +1,73 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/Hilo.php';
+require_once __DIR__.'/includes/Entrada.php';
+require_once __DIR__.'/includes/Usuario.php';
 
 $tituloPagina = 'Hilo';
+$contenidoPrincipal = "HOLA";
 
 if(isset($_GET['hilo'])){
-	$numero = $_GET['hilo'];
-
-	$contenidoPrincipal = <<<EOS
-	<h1>Aqui iria la discusion del hilo. En este caso el hilo: $numero</h1>
-	EOS;
+	$id_hilo = $_GET['hilo'];
+	$hilo = es\ucm\fdi\aw\Hilo::getHilo($id_hilo);
+	if ($hilo != null){
+		
+		$autor=es\ucm\fdi\aw\Usuario::buscaPorID($hilo->getID_usuario());
+		$nombreAutor=$autor->getNombre();
+		$apellidoAutor= $autor->getApellido();
+		$idAutor=$hilo->getID_usuario();
+		$comentarioAutor=$hilo->getComentario();
+		$fechaAutor=$hilo->getFecha();
+		$tituloHilo = $hilo->getTitulo();
+		$comienzoHilo = <<<EOS
+			<h2>$tituloHilo</h2>
+			<div>
+				<div>
+					<p> <a href = "perfil_user.php?id=$idAutor">$nombreAutor $apellidoAutor</a> ha dicho: </p>
+				</div>
+				<div>
+					<p> $comentarioAutor </p>
+				</div>
+			</div>
+EOS;
+		
+		$entradas=es\ucm\fdi\aw\Entrada::getEntradasPorHilo($id_hilo);
+		if($entradas != null)$cantEntradas = count($entradas); 
+		else $cantEntradas=0;
+		$i = 0;
+		$respuestas = "";
+		for($i = 0; $i < $cantEntradas; $i++){
+			$usuario=es\ucm\fdi\aw\Usuario::buscaPorID($entradas[$i]->getID_usuario());
+			$nombreUsuario=$usuario->getNombre();
+			$apellidoUsuario= $usuario->getApellido();
+			$idUsuario=$usuario->getID();
+			$comentarioUsuario=$entradas[$i]->getComentario();
+			$fechaUsuario=$entradas[$i]->getFecha();
+			$respuestas.=<<<EOS
+			<br>
+				<div>
+					<div>
+						<p> <a href = "perfil_user.php?id=$idUsuario">$nombreUsuario $apellidoUsuario</a> ha dicho: </p>
+					</div>
+					<div>
+						<p> $comentarioUsuario </p>
+					</div>
+				</div>					
+EOS;
+		}
+		
+		$contenidoPrincipal=<<<EOS
+			$comienzoHilo	
+			$respuestas
+EOS;
+		
+	}else $contenidoPrincipal = "<h1> ERROR CON EL HILO </h1>";
 }
 else{
 	$contenidoPrincipal = <<<EOS
 	<h1>No se está pasando el numero del hilo</h1>
-	EOS;
+EOS;
 }
 
 require __DIR__.'/includes/plantillas/plantilla.php';
