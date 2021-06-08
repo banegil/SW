194dd51c75178258a2a0840f80b4263d37ecb20f
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-09 00:50:36 +0200

    Add files via upload

--- unipet_web/foro.php
+++ unipet_web/foro.php
@@ -2,12 +2,19 @@
 
 require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/comun/listaHilos.php';
+require_once ("includes/usuarioUtils.php");
 
 $tituloPagina = 'Foro';
 $hilos = listaHilos(es\ucm\fdi\aw\Hilo::getHilos());
+$formNuevoForo="";
+if(estaLogado()){
+$form=new es\ucm\fdi\aw\FormularioHilo(idUsuarioLogado());	
+$formNuevoForo=$form->gestiona();
+}
 
 $contenidoPrincipal = <<<EOS
 $hilos
+$formNuevoForo
 EOS;
 
 require __DIR__.'/includes/plantillas/plantilla.php';
