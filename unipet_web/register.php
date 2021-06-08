7a432ba606db1357538019e09d5167f9f9249fc2
banegil <64730977+banegil@users.noreply.github.com>
2021-06-08 19:34:03 +0200

    Update register.php

--- unipet_web/register.php
+++ unipet_web/register.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__.'\includes\config.php';
-require_once __DIR__.'\includes\FormularioRegistro.php';
 
 $form = new FormularioRegistro("1");
 $htmlFormRegistro = $form->gestiona();
