83a8dbeb572115a5eea155067d101b30e2644855
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-08 13:19:17 +0200

    Add files via upload

--- unipet_web/includes/Usuario.php
+++ unipet_web/includes/Usuario.php
@@ -1,19 +1,15 @@
 <?php
-
 namespace es\ucm\fdi\aw;
-
 class Usuario
 {
-	
-	
-	 public static function actualiza($dni, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion){
-        $usuario = new Usuario($dni, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion, NULL, NULL);
+	 public static function actualiza($id, $dni, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion){
+        $usuario = new Usuario($id, $dni, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion, NULL, NULL);
         return $usuario;
     }
 
-	public static function login($DNI, $contraseña)
+	public static function login($dni, $contraseña)
 	{
-		$user = self::buscaPorDNI($DNI);
+		$user = self::buscaPorDNI($dni);
 		if ($user && $user->compruebaContraseña($contraseña)) {
 		return $user;
 		}    
@@ -33,18 +29,17 @@ class Usuario
                                 $fila['apellido'], $fila['telefono'], $fila['email'],
                                 $fila['contraseña'], $fila['nacimiento'], 
                                 $fila['direccion'], $fila['tipo'], $fila['creacion']);
-			//$user->id = $conn->insert_id;
         $rs->free();
         return $user;
        }
        return false;
     }
 	
-	 public static function buscaPorID($idUsuario)
+    public static function buscaPorID_usuario($ID_Usuario)
     {
 	   $app = Aplicacion::getSingleton();
        $conn = $app->conexionBd();
-       $query = sprintf("SELECT * FROM usuarios WHERE ID=%d ", $idUsuario); 
+       $query = sprintf("SELECT * FROM usuarios WHERE ID=%d ", $ID_Usuario); 
        $rs = $conn->query($query);
        if($rs && $rs->num_rows == 1){
            $fila = $rs->fetch_assoc();
@@ -53,20 +48,19 @@ class Usuario
                                 $fila['apellido'], $fila['telefono'], $fila['email'],
                                 $fila['contraseña'], $fila['nacimiento'], 
                                 $fila['direccion'], $fila['tipo'], $fila['creacion']);
-			//$user->id = $conn->insert_id;
         $rs->free();
         return $user;
        }
        return false;
     }
 
-    public static function register($DNI, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion,$creacion)
+    public static function register($ID_usuario, $DNI, $nombre, $apellido, $telefono, $email, $contraseña, $nacimiento, $direccion,$creacion)
     {	
 		$user = self::buscaPorDNI($DNI);
 		if ($user) {
 			return false;
 		}
-		$user = new Usuario(0,$DNI, $nombre, $apellido, $telefono, $email, self::hashPassword($contraseña), $nacimiento, $direccion, "normal", $creacion);
+		$user = new Usuario($ID_usuario, $DNI, $nombre, $apellido, $telefono, $email, self::hashPassword($contraseña), $nacimiento, $direccion, "normal", $creacion);
 		return self::inserta($user);
   }
 
@@ -79,9 +73,9 @@ class Usuario
     {
         $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query=sprintf("INSERT INTO usuarios (DNI, nombre, apellido, telefono, email, contraseña, nacimiento, direccion, tipo, creacion)
-			VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
-            , $conn->real_escape_string($usuario->dni)
+        $query=sprintf("INSERT INTO Usuarios (DNI, nombre, apellido, telefono, email, contraseña, nacimiento, direccion, tipo, creacion)
+			VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
+            , $conn->real_escape_string($usuario->DNI)
             , $conn->real_escape_string($usuario->nombre)
             , $conn->real_escape_string($usuario->apellido)
             , $conn->real_escape_string($usuario->telefono)
@@ -108,17 +102,18 @@ class Usuario
 	$app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
         $contraseña = password_hash($usuario->contraseña, PASSWORD_DEFAULT);
-      $query = sprintf("UPDATE usuarios SET  dni = '%s', nombre = '%s', apellido = '%s', telefono = %d, email = '%s', contraseña = '%s', nacimiento = '%s', direccion = '%s' WHERE id = %d"
+      $query = sprintf("UPDATE Usuarios SET  dni = '%s', nombre = '%s', apellido = '%s', telefono = %d, email = '%s', contraseña = '%s', nacimiento = '%s', direccion = '%s' WHERE dni = %d"
 	
+	  , $usuario->ID_usuario
 	  , $usuario->dni
-    , $usuario->nombre
+      , $usuario->nombre
 	  , $usuario->apellido
 	  , $usuario->telefono
 	  , $usuario->email
 	  , $contraseña
 	  , $usuario->nacimiento
 	  , $usuario->direccion
-	  , $usuario->id);
+	  , $usuario->dni);
 	$result = $conn->query($query);
 	if (!$result) {
 	  error_log($conn->error);  
@@ -127,7 +122,6 @@ class Usuario
 	}
 	return $result;
 }    
-
 
 private $id;
 private $dni;
@@ -141,11 +135,11 @@ private $direccion;
 private $tipo;
 private $creacion;
 
-private function __construct($id,$dni, $nombre, $apellido, $telefono,
+private function __construct($ID_usuario, $dni, $nombre, $apellido, $telefono,
                              $email, $contraseña, $nacimiento, $direccion , $tipo, $creacion)
 {
-	$this->id = $id;
-    $this->dni = $dni;
+    $this->id = $id;
+	$this->dni = $dni;
     $this->nombre = $nombre;
     $this->apellido = $apellido;
     $this->telefono = $telefono;
@@ -167,16 +161,20 @@ public function cambiacontraseña($nuevocontraseña)
 {
   $this->contraseña = password_hash($nuevocontraseña, PASSWORD_DEFAULT);
 }
+
+/**
+ * Get the value of tipo
+ */ 
+public function getID_usuario()
+{
+return $this->ID_usuario;
+}
 /**
  * Get the value of tipo
  */ 
 public function getTipo()
 {
 return $this->tipo;
-}
-	
-public function getContraseña(){
-  return $this->contraseña;
 }
 /**
  * Get the value of ID
@@ -276,13 +274,6 @@ public function setDireccion($direccion)
 $this->direccion = $direccion;
 
 return $this;
-}
-public function actualizarUser(){
-  $respuesta = false;
-  if (self::buscaPorDNI($this->dni)) {
-  $respuesta = self::actualizaUsuario($this);
-  } 
-  return $respuesta;
 }
 
 
