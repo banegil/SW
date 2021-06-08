72d95c13881fcf520f95d4505ef3528f04530855
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 00:54:07 +0200

    Update addAnimal.php

--- unipet_web/addAnimal.php
+++ unipet_web/addAnimal.php
@@ -1,59 +1,16 @@
-
 <?php
-		require_once __DIR__.'\includes\config.php';
 
-$tituloPagina = "Añadir Animal";
-$contenidoPrincipal = <<<EOS
-			<h1>Nueva mascota: </h1>
-			<form id="formAddAnimal" action="./procesarAddAnimal.php" method="POST">
-			<fieldset>
-				<legend>Rellene los datos del animal</legend>
-				
-				<div><label>Número de indentificación:</label> 
-					<input type="number" name="idAnimal" value="" size="25"/>
-				</div>	
-				
-				<div><label>Nombre:</label> 
-					<input type="text" name="nameAnimal" value="" size="25"/>
-				</div>
-				
-				<div><label>Nacimiento:</label> 
-					<input type="date" name="fechaNacimiento" value="" size="25"/>
-				</div>
-				
-				<div><label>Tipo:</label> 
-					<select name="type">
-					  <option value="perro" selected>Perro</option>
-					  <option value="gato">Gato</option>
-					</select>
-				</div>
-				
-				<div><label>Raza:</label> 
-					<input type="text" name="raza" value="" size="25"/>
-				</div>
-				
-				<div><label>Sexo:</label> 
-					<select name="sexo">
-					  <option value="macho" selected>Macho</option>
-					  <option value="hembra">Hembra</option>
-					</select>
-				</div>
-				
-				<div><label>Peso:</label> 
-					<input type="number" name="peso" value="0" size="20"/>
-				</div>
-				
-				<div><label>Ingreso:</label> 
-					<input type="date" name="fechaIngreso" value="" size="25"/>
-				</div>
-				
-				<div><label>Protectora:</label> 
-					<input type="text" name="protectora" value="" size="25"/>
-				</div>
-				
-				<div><button type="submit">Añadir</button> <button type="reset">Borrar todo</button></div>
-			</fieldset>
+require_once __DIR__.'/includes/config.php';
+
+$form = new es\ucm\fdi\aw\FormularioAddAni("1");
+$htmlFormAddAni = $form->gestiona();
+
+$tituloPagina = 'Add ani';
+
+$contenidoPrincipal = "<h1>Añadir animal</h1>";
+
+$contenidoPrincipal .= <<<EOS
+$htmlFormAddAni
 EOS;
+
 require_once __DIR__.'/includes/plantillas/plantilla.php';
-
-
