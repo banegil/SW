02cedab2def36e12c701310cc678cb8615a2f4fc
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 01:29:27 +0200

    Update solicitud.php

--- unipet_web/solicitud.php
+++ unipet_web/solicitud.php
@@ -1,38 +1,31 @@
 <?php 
 	require_once __DIR__.'/includes/config.php';
+	require_once __DIR__.'/includes/usuarioUtils.php';
 	
-	$dni = $_GET['dni'];
-	$id = $_GET['id'];
+	$idAni = $_GET['idAni'];
+	$idUsu = $_GET['idUsu'];
+	
 	$tituloPagina = "Solicitud";
-	$contract = es\ucm\fdi\aw\Contrato::buscaPorDNIeID($dni, $id);
-	$animal = es\ucm\fdi\aw\Animal::buscaPorID($id);
-	$usuario = es\ucm\fdi\aw\Usuario::buscaPorID_usuario($dni);
+	
+	$contract = es\ucm\fdi\aw\Contrato::buscaPorIDeID($idUsu, $idAni);
+	$animal = es\ucm\fdi\aw\Animal::buscaPorID($idAni);
+	$usuario = es\ucm\fdi\aw\Usuario::buscaPorID_usuario($idUsu);
 
 	$contenidoPrincipal = '<h3>Estado de la solicitud: '.$contract->getEstado().'</h3>';
 	$contenidoPrincipal .= '<h3>Nombre del usuario: '.$usuario->getNombre().' '.$usuario->getApellido().'</h3>';
-	$contenidoPrincipal .= '<h3>Nombre animal: <a href = "perfil_animal.php?id='.$id.'">'.$animal->getNombre().'</a></h3>';
+	$contenidoPrincipal .= '<h3>Nombre animal: <a href = "perfil_animal.php?id='.$idAni.'">'.$animal->getNombre().'</a></h3>';
 	$contenidoPrincipal .= '<h3>Formulario enviado: '.$contract->getFormulario().'</h3>';
 	$contenidoPrincipal .= '<h3>Fecha de la solicitud: '.$contract->getFecha().'</h3>';
 	
-	if($_SESSION['tipo'] == "voluntario" || $_SESSION['tipo'] == "administrador"){
-		$contenidoPrincipal .= '<form id="modEstado" action="./procesarCambio.php" method="POST">';
-			$_SESSION['idContrato'] = $id;
-			$_SESSION['dniContrato'] = $dni;
-			
-			$contenidoPrincipal .= <<<EOS
-					<div><label>Estado: </label>
-					<select name="estado">
-				  <option value="EnTramite" selected>En tramite</option>
-				  <option value="FaltDatos">Faltan datos</option>
-				  <option value="PendCita">Pendiente de cita</option>
-				  <option value="EsperaRes">Esperar respuesta</option>
-				  <option value="Rechazado">Rechazar</option>
-				  <option value="Aprobado">Aprobar</option>
-				</select>
-				<button type="submit">Enviar</button>
-			</div>
-		</form>
+	if(permisosVoluntario()){
+		
+		$form = new es\ucm\fdi\aw\FormularioCambio($idAni, $idUsu);
+		$htmlFormCambio = $form->gestiona();
+		
+		$contenidoPrincipal .=<<<EOS
+		$htmlFormCambio
 EOS;
+
 	}
 	require __DIR__.'/includes/plantillas/plantilla.php';
 ?>
