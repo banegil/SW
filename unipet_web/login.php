1d4a152768818c3c67dabe3fb5d141523daac154
banegil <64730977+banegil@users.noreply.github.com>
2021-06-08 02:09:56 +0200

    Update login.php

--- unipet_web/login.php
+++ unipet_web/login.php
@@ -1,14 +1,14 @@
 <?php
+require_once "includes\FormularioLogin.php";
 require_once __DIR__.'/includes/config.php';
 
-$form = new es\ucm\fdi\aw\FormularioLogin("1");
+$form = new FormularioLogin("1");
 $htmlFormLogin = $form->gestiona();
 
 $tituloPagina = 'Login';
 
 $contenidoPrincipal = <<<EOS
-<h1>Acceso al sistema</h1>
 $htmlFormLogin
 EOS;
 
-require __DIR__.'/includes/plantillas/plantilla.php';
+require __DIR__.'/includes/plantillas/plantillaLogin.php';
