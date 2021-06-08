7a4abdd1f74381f896edc2e773a770791a7a6b25
rcincea <49287636+rcincea@users.noreply.github.com>
2021-06-08 22:25:02 +0200

    Update perfil_user.php

--- unipet_web/perfil_user.php
+++ unipet_web/perfil_user.php
@@ -24,30 +24,39 @@ if($usuario != null){ // comprobamos si el usuario esta en la base de datos
 	if ($usuario->getID() == idUsuarioLogado() || permisosVoluntario()) $esMiPerfil = true;
     // aqui iria el div para foto nombre y apellido
     $contenidoPrincipal .= "<div id='contenedorNombre'>";
-    $contenidoPrincipal .= "<div id='contenedorNameApellido'>";
-	if(file_exists(FICHERO_IMGUSU.'/'.$usuario->getID().'.jpg')){
+    $contenidoPrincipal .= "<div id='contenedorImagen'>";
+    if(file_exists(FICHERO_IMGUSU.'/'.$usuario->getID().'.jpg')){
 		$contenidoPrincipal .= "<img class='perfilUser' src=img/usu/".$usuario->getID().".jpg alt=Foto usuario".$usuario->getID()."/>";
 		if($esMiPerfil || permisosVoluntario()){
+            $contenidoPrincipal .= "<div id='cambiarFotoContenedor'>";
 			$contenidoPrincipal .="<p>Cambiar foto:</p>";
 			$form = new es\ucm\fdi\aw\FormularioFotoUsu($usuario->getID());
 			$contenidoPrincipal .= $form->gestiona();
+            $contenidoPrincipal .= "</div>";
 		}
 	}else{
 		$contenidoPrincipal .= "<img class='perfilUser' src=img/usu/null.jpg alt=Foto usuario".$usuario->getID()."/>";
-		if($esMiPerfil || permisosVoluntario()){
+        $contenidoPrincipal .= "<div id='cambiarFotoContenedor'>";
+        if($esMiPerfil || permisosVoluntario()){
 			$contenidoPrincipal .="<p>Añadir foto:</p>";
 			$form = new es\ucm\fdi\aw\FormularioFotoUsu($usuario->getID());
 			$contenidoPrincipal .= $form->gestiona();
+            $contenidoPrincipal .= "</div>";
 		}
 	}
-	if ($esMiPerfil)$contenidoPrincipal .="<p> DNI: ".$usuario->getDni(). "</p>";
-    $contenidoPrincipal .="<p> NOMBRE: ".$usuario->getNombre(). "</p>";
-	$contenidoPrincipal .="<p> APELLIDO: ".$usuario->getApellido(). "</p>";
+    $contenidoPrincipal .= "</div>";
+    $contenidoPrincipal .= "<div id='contenedorNameApellido'>";
+	
+	
+    $contenidoPrincipal .="<p> ".$usuario->getNombre(). "</p>";
+	$contenidoPrincipal .="<p> ".$usuario->getApellido(). "</p>";
+    
     $contenidoPrincipal .= "</div>";
     $contenidoPrincipal .= "</div>";
     // contenedor con el resto de datos
     $contenidoPrincipal .= "<div id='contenedorDatos'>";
     $contenidoPrincipal .= "<div id='contenedorDatosIzquierda'>";
+    if ($esMiPerfil)$contenidoPrincipal .="<p> DNI: ".$usuario->getDni(). "</p>";
     if ($esMiPerfil)$contenidoPrincipal .= "<p> TELEFONO: ".$usuario->getTelefono(). "</p>";
     if ($esMiPerfil)$contenidoPrincipal .= "<p> EMAIL: ".$usuario->getEmail(). "</p>";
     $contenidoPrincipal .= "<p> FECHA NACIMIENTO : ".$usuario->getNacimiento(). "</p>";
