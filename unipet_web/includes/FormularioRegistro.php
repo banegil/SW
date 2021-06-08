f5f6e72ae779d8514e8b29f1d11a3651a40980f8
banegil <64730977+banegil@users.noreply.github.com>
2021-06-08 17:35:16 +0200

    Update FormularioRegistro.php

--- unipet_web/includes/FormularioRegistro.php
+++ unipet_web/includes/FormularioRegistro.php
@@ -9,74 +9,68 @@ class FormularioRegistro extends Form
     
     protected function generaCamposFormulario($datos, $errores = array())
     {
-		$ID_usuario = $datos['ID_usuario'] ?? '';
-        	$DNI = $datos['DNI'] ?? '';
+        $DNI = $datos['DNI'] ?? '';
 		$nombre = $datos['nombre'] ?? '';
 		$apellido = $datos['apellido'] ?? '';
 		$telefono = $datos['telefono'] ?? '';
-		$email = $datos['e-mail'] ?? '';
+		$email = $datos['email'] ?? '';
 		$nacimiento = $datos['nacimiento'] ?? '';
 		$direccion = $datos['direccion'] ?? '';
-
-        // Se generan los mensajes de error si existen.
-		/*
-        $htmlErroresGlobales = self::generaListaErroresGlobales($errores);
-        $errorDNI = self::createMensajeError($errores, 'DNI', 'span', array('class' => 'error'));
-        $errorNombre = self::createMensajeError($errores, 'Nombre', 'span', array('class' => 'error'));
-        $errorApellido = self::createMensajeError($errores, 'Apellido', 'span', array('class' => 'error'));
-        $errorTelefono = self::createMensajeError($errores, 'Teléfono', 'span', array('class' => 'error'));
-		$errorEmail = self::createMensajeError($errores, 'E-mail', 'span', array('class' => 'error'));
-		$errorContraseña = self::createMensajeError($errores, 'Contraseña', 'span', array('class' => 'error'));
-		$errorContraseña2 = self::createMensajeError($errores, 'Contraseña2', 'span', array('class' => 'error'));
-		$errorNacimiento = self::createMensajeError($errores, 'Naciemiento', 'span', array('class' => 'error'));
-		$errorDireccion = self::createMensajeError($errores, 'Direccion', 'span', array('class' => 'error'));
-		*/
+		
         $html = <<<EOF
             <fieldset>
-		  <div class="form">
-			<h2>Registro</h2>
-			<div class="input">
-			  <div class="inputBox">
-				<label>DNI</label>
-				<input class"control" type="text" name="DNI" value="$DNI" placeholder="00000000A">
-			  </div>
-			  <div class="inputBox">
-				<label>Nombre</label>
-				<input class"control" type="text" name="nombre" value="$nombre" placeholder="Juan">
-			  </div>
-			  <div class="inputBox">
-				<label>Apellido</label>
-				<input class"control" type="text" name="apellido" value="$apellido" placeholder="Perez">
-			  </div>
-			  <div class="inputBox">
-				<label>Teléfono</label>
-				<input class"control" type="number" name="telefono" value="$telefono" placeholder="614876453">
-			  </div>
-			  <div class="inputBox">
-				<label>E-mail</label>
-				<input class"control" type="email" name="email" value="$email" placeholder="jperez@gmail.com">
-			  </div>
-			  <div class="inputBox">
-				<label>Contraseña</label>
-				<input class"control" type="password" name="contraseña" placeholder="********">
-			  </div>
-			  <div class="inputBox">
-				<label>Vueleve a introducir la contraseña</label>
-				<input class"control" type="password" name="contraseña2" placeholder="********">
-			  </div>
-			  <div class="inputBox">
-				<label>Fecha de nacimiento</label>
-				<input class"control" type="date" name="nacimiento" value="$nacimiento" placeholder="12/01/1999">
-			  </div>
-			  <div class="inputBox">
-				<label>Dirección</label>
-				<input class"control" type="text" name="direccion" value="$direccion" placeholder="Calle Mayor N7">
-			  </div>
-			  <div class="inputBox">
-				<input type="submit" name="" value="Registrarse">
-			  </div>
-			</div>
-		  </div>
+					<head>
+					  <meta charset="UTF-8" />
+					  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+					  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+					</head>
+
+					<body>
+					  <div class="form">
+						<h2>Registro</h2>
+						<div class="input">
+						  <div class="inputBox">
+							<label>DNI</label>
+							<input class"control" type="text" name="DNI" value="$DNI" placeholder="00000000A">
+						  </div>
+						  <div class="inputBox">
+							<label>Nombre</label>
+							<input class"control" type="text" name="nombre" value="$nombre" placeholder="Juan">
+						  </div>
+						  <div class="inputBox">
+							<label>Apellido</label>
+							<input class"control" type="text" name="apellido" value="$apellido" placeholder="Perez">
+						  </div>
+						  <div class="inputBox">
+							<label>Teléfono</label>
+							<input class"control" type="number" name="telefono" value="$telefono" placeholder="614876453">
+						  </div>
+						  <div class="inputBox">
+							<label>E-mail</label>
+							<input class"control" type="email" name="email" value="$email" placeholder="jperez@gmail.com">
+						  </div>
+						  <div class="inputBox">
+							<label>Contraseña</label>
+							<input class"control" type="password" name="contraseña" placeholder="********">
+						  </div>
+						  <div class="inputBox">
+							<label>Vueleve a introducir la contraseña</label>
+							<input class"control" type="password" name="contraseña2" placeholder="********">
+						  </div>
+						  <div class="inputBox">
+							<label>Fecha de nacimiento</label>
+							<input class"control" type="date" name="nacimiento" value="$nacimiento" placeholder="">
+						  </div>
+						  <div class="inputBox">
+							<label>Dirección</label>
+							<input class"control" type="text" name="direccion" value="$direccion" placeholder="Calle Mayor N7">
+						  </div>
+						  <div class="inputBox">
+							<input type="submit" name="" value="Registrarse">
+						  </div>
+						</div>
+					  </div>
+					</body>
             </fieldset>
 EOF;
         return $html;
@@ -90,9 +84,9 @@ EOF;
 		$letra = substr($DNI, -1);
 		$numeros = substr($DNI, 0, -1);
 		
-	if (empty($DNI) || substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) == $letra || strlen($letra) != 1 || strlen ($numeros) != 8 ){
-		$result['DNI'] = "El DNI tiene que tener 8 números y una letra al final.";
-	}
+		if (empty($DNI) || substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) == $letra || strlen($letra) != 1 || strlen ($numeros) != 8 ){
+			$result['DNI'] = "El DNI tiene que tener 8 números y una letra al final.";
+		}
         
         $nombre = $datos['nombre'] ?? null;
         if ( empty($nombre) || mb_strlen($nombre) < 2 ) {
@@ -110,7 +104,7 @@ EOF;
         }
 		
 		// El e-mail ya viene filtrado por type="email"
-		$email = $datos['e-mail'] ?? null;
+		$email = $datos['email'] ?? null;
 		
 		$contraseña = $datos['contraseña'] ?? null;
         if ( empty($contraseña) || mb_strlen($contraseña) < 5 ) {
