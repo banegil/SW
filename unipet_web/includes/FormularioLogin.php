75920e396b66d0645e26caa19baea0690df142e3
banegil <64730977+banegil@users.noreply.github.com>
2021-06-08 17:44:55 +0200

    Update FormularioLogin.php

--- unipet_web/includes/FormularioLogin.php
+++ unipet_web/includes/FormularioLogin.php
@@ -4,26 +4,34 @@
 	class FormularioLogin extends Form {
 
 		protected function generaCamposFormulario($datos, $errores = array())
-	    {
+	    {			
 	    	$html = <<<EOS
-				<fieldset>
-				  <div class="form">
-					<h2>Login</h2>
-					<div class="input">
-					  <div class="inputBox">
-						<label>DNI del usuario</label>
-						<input type="text" name="ID_usuario" placeholder="ejemplo@gmail.com">
-					  </div>
-					  <div class="inputBox">
-						<label>Contraseña</label>
-						<input type="password" name="contraseña" placeholder="******">
-					  </div>
-					  <div class="inputBox">
-						<input type="submit" name="" value="Entrar">
-					  </div>
-					</div>
-				  </div>
-				</fieldset>
+					<fieldset>
+						<head>
+						  <meta charset="UTF-8" />
+						  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+						  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+						</head>
+
+						<body>
+						  <div class="form">
+							<h2>Login</h2>
+							<div class="input">
+							  <div class="inputBox">
+								<label>Nombre de usuario</label>
+								<input class="control" type="text" name="DNI" placeholder="00000000A">
+							  </div>
+							  <div class="inputBox">
+								<label>Contraseña</label>
+								<input type="password" name="contraseña" placeholder="******">
+							  </div>
+							  <div class="inputBox">
+								<input type="submit" name="login" value="Entrar">
+							  </div>
+							</div>
+						  </div>
+						</body>
+					</fieldset>
 EOS;
 	    	return $html;
 	  	}
@@ -31,10 +39,10 @@ EOS;
 	  	protected function procesaFormulario($datos)
 	    {
 	    	$errores = array();
-	    	$ID_usuario = isset($_POST['ID_usuario']) ? $_POST['ID_usuario'] : null;
+	    	$DNI = isset($_POST['DNI']) ? $_POST['DNI'] : null;
 
-			if ( empty($ID_usuario) ) {
-				$errores[] = "El DNI de usuario no puede estar vacío";
+			if ( empty($DNI) ) {
+				$errores[] = "El DNI no puede estar vacío";
 			}
 
 			$pass = isset($_POST['contraseña']) ? $_POST['contraseña'] : null;
@@ -43,18 +51,17 @@ EOS;
 			}
 
 			if (count($errores) === 0) {
-				$usuario = Usuario::login($ID_usuario,$pass);
+				$usuario = Usuario::login($DNI,$pass);
 				if($usuario){
 					$_SESSION['login'] = true;
-					$_SESSION['ID_usuario'] = $usuario->getID();
-					$_SESSION['DNI'] = $usuario->getDNI();
+					$_SESSION['DNI'] = $DNI;
 					$_SESSION['tipo'] = $usuario->getTipo();
 					$_SESSION['nombre'] = $usuario->getNombre();
 					$_SESSION['id'] = $usuario->getID();
 					$errores = 'index.php';
 				}
 				else{ 
-					$errores[]="Usuario o contraseña no validos";
+					$errores[]="DNI o contraseña no validos";
 				}
 				
 			}
