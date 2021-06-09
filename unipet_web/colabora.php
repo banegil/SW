f3fd78603582bdd688dec3be9adea2a4f4ebba86
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 02:50:52 +0200

    Update colabora.php

--- unipet_web/colabora.php
+++ unipet_web/colabora.php
@@ -1,15 +1,24 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
-
-$form = new es\ucm\fdi\aw\FormularioColabora("1");
-$htmlFormColabora = $form->gestiona();
+require_once ("includes/usuarioUtils.php");
 
 $tituloPagina = 'Colabora';
 
-$contenidoPrincipal = <<<EOS
-<h1>Colabora</h1>
-$htmlFormColabora
+if(!estaLogado()){
+	$contenidoPrincipal = <<<EOS
+	<h1>Para colaborar con nosotros necesitamos que se registre primero <a href = "register.php">aquí</a>. o si ya tiene cuenta loguearse <a href = "login.php">aquí</a>.</h1>
 EOS;
+}
+else{
+	
+	$form = new es\ucm\fdi\aw\FormularioColabora("1");
+	$htmlFormColabora = $form->gestiona();
+	
+	$contenidoPrincipal = <<<EOS
+	<h1>Voluntariado</h1>
+	$htmlFormColabora
+EOS;
+}
 
 require __DIR__.'/includes/plantillas/plantilla.php';
