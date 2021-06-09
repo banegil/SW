8e5c5ef59ab7b3c74f1caf048f64ebcc4196249b
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 02:50:14 +0200

    Update FormularioColabora.php

--- unipet_web/includes/FormularioColabora.php
+++ unipet_web/includes/FormularioColabora.php
@@ -1,36 +1,23 @@
 <?php
 
 namespace es\ucm\fdi\aw;
+require_once ("includes/usuarioUtils.php");
 
 class FormularioColabora extends Form
 {
-    public function __construct() {
-        parent::__construct('formColabora');
-    }
     
     protected function generaCamposFormulario($datos, $errores = array())
     {
 	    	$html = <<<EOS
 					<fieldset>
-            <legend>Cómo colaborar con nosotros</legend>
-			<form action="mailto:unipetcontact@gmail.com" method="get" enctype="text/plain">
-				  <div>
-					<p><label></label> <input type="text" name="comoColaborar"/></p>
-				  </div>
-				  <div>
-					<p><label>E-mail</label> <input type="email" name="email" /></p>
-				  </div>
-				  <div>
-					<p><label>Nombre</label> <input type="text" name="nombre" /></p>
-				  </div>
-				  <div>
-					<p><label>Apellidos</label> <input type="text" name="apellidos" /></p>
-				  </div>
-				  <div>
-					<input type="submit" name="submit" value="Send" />
-					<input type="reset" name="reset" value="Clear" />
-				  </div>
-			</form>
+					<legend>Si quiere presentarse como voluntario, dejenos a continuacion una solicitud diciendonos sus motivaciones para tal acto</legend>
+					  <div>
+						<label>Motivaciones</label> <input type="text" name="voluntariado" size="200"/>
+					  </div>
+					  <div>
+						<input type="submit" name="submit" value="Send" />
+						<input type="reset" name="reset" value="Clear" />
+					  </div>
 					</fieldset>
 EOS;
 			return $html;
@@ -38,6 +25,20 @@ EOS;
     
 
     protected function procesaFormulario($datos) {
-		return 'colabora.php';
+		
+		$result = array();
+		
+        $voluntariado = $datos['voluntariado'] ?? null;
+		
+		if(mb_strlen($voluntariado) < 10){
+			$result[] = 'Por favor, indiquenos con más extension cuales son sus motivaciones';
+		}else{
+			$comentario = Colabora::add(idUsuarioLogado(), $voluntariado);
+			Colabora::inserta($comentario);
+			$result = 'index.php';
+		}
+		
+		
+		return $result;
 	}
 }
