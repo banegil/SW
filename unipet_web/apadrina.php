3c557d1b276592bb4294b6aaf8e533d2cb8f6562
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 05:55:42 +0200

    Update apadrina.php

--- unipet_web/apadrina.php
+++ unipet_web/apadrina.php
@@ -1,39 +1,21 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
+require_once ("includes/usuarioUtils.php");
 
-$form = new es\ucm\fdi\aw\FormularioApadrina("1");
+$idAni = $_GET['id'];
+
+$animal = es\ucm\fdi\aw\Animal::buscaPorID($idAni);
+$nombre = $animal->getNombre();
+
+$form = new es\ucm\fdi\aw\FormularioApadrina(idUsuarioLogado(), $idAni);
 $htmlFormApadrina = $form->gestiona();
 
 $tituloPagina = 'Apadrina';
 
 $contenidoPrincipal = <<<EOS
-        <h2>
-			<span>&nbsp&nbsp</span>
-			<span>&nbsp&nbsp</span>
-			<span>&nbsp&nbsp</span>
-			<span>&nbsp&nbsp</span>
-            <span>A</span>
-            <span>P</span>
-            <span>A</span>
-            <span>D</span>
-            <span>R</span>
-            <span>I</span>
-            <span>N</span>
-            <span>A</span>
-			<span>&nbsp&nbsp</span>
-            <span>C</span>
-            <span>O</span>
-            <span>N</span>
-            <span>&nbsp&nbsp</span
-            <span>U</span>
-            <span>N</span>
-            <span>I</span
-            <span>P</span>
-            <span>E</span>
-            <span>T</span>
-        </h2>
-$htmlFormApadrina
+   <h1>Apadrinando a {$nombre}. Gracias por su colaboracion :D</h1>
+	$htmlFormApadrina
 EOS;
 
 require __DIR__.'/includes/plantillas/plantillaApadrina.php';
