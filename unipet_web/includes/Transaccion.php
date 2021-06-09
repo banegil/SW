3e3eeb814aa2a656b5df430f4e9e2c61f0477023
K3KO-UCM <49587278+K3KO-UCM@users.noreply.github.com>
2021-06-09 07:06:26 +0200

    Update Transaccion.php

--- unipet_web/includes/Transaccion.php
+++ unipet_web/includes/Transaccion.php
@@ -12,7 +12,7 @@ class Transaccion
        $rs = $conn->query($query);
        if($rs && $rs->num_rows == 1){
            $fila = $rs->fetch_assoc();
-           $user = new Transaccion($fila['ID'], $fila['ID_usuario'], $fila['tarjeta'], $fila['cantidad']);
+           $user = new Transaccion($fila['ID'], $fila['ID_usuario'], $fila['cantidad'], $fila['tarjeta'], $fila['ID_animal']);
         $rs->free();
         return $user;
        }
@@ -27,30 +27,55 @@ class Transaccion
        $rs = $conn->query($query);
        if($rs && $rs->num_rows == 1){
            $fila = $rs->fetch_assoc();
-           $user = new Transaccion($fila['ID'], $fila['ID_usuario'], $fila['tarjeta'], $fila['cantidad']);
+           $user = new Transaccion($fila['ID'], $fila['ID_usuario'], $fila['cantidad'], $fila['tarjeta'], $fila['ID_animal']);
         $rs->free();
         return $user;
        }
        return false;
     }
 
-    public static function register($usuario, $cantidad, $numero_tarjeta)
+    public static function register($usuario, $cantidad, $numero_tarjeta, $animal)
     {	
 		//Guardamos $protectora como la fecha-h-m-s-microsegundo(6 decimales)
 		//$date = DateTime::createFromFormat('U.u', microtime(TRUE));
 		//$protectora = $date->format('YmdHisu');
 
-		$user = new Transaccion(0, $usuario, $numero_tarjeta, $cantidad);
-		return self::inserta($user);
+		$user = new Transaccion(0, $usuario, $cantidad, $numero_tarjeta, $animal);
+		
+		if($animal == NULL){
+			return self::insertaConNull($user);
+		}
+		else{
+			return self::inserta($user);
+		}
     }
     
     private static function inserta($transaccion)
     {
         $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query=sprintf("INSERT INTO transacciones (ID, ID_usuario, tarjeta, cantidad)
+        $query=sprintf("INSERT INTO transacciones (ID_usuario, tarjeta, cantidad, ID_animal)
 			VALUES('%s', '%s', '%s', '%s')"
-            , $conn->real_escape_string($transaccion->ID)
+            , $conn->real_escape_string($transaccion->ID_usuario)
+            , $conn->real_escape_string($transaccion->numero_tarjeta)
+            , $conn->real_escape_string($transaccion->cantidad)
+			, $conn->real_escape_string($transaccion->ID_animal));
+        if ( $conn->query($query) ) {
+            $transaccion->ID = $conn->insert_id;
+		}
+		else {
+            echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $transaccion;
+    }
+	
+	private static function insertaConNull($transaccion)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query=sprintf("INSERT INTO transacciones (ID_usuario, tarjeta, cantidad)
+			VALUES('%s', '%s', '%s')"
             , $conn->real_escape_string($transaccion->ID_usuario)
             , $conn->real_escape_string($transaccion->numero_tarjeta)
             , $conn->real_escape_string($transaccion->cantidad));
@@ -63,44 +88,20 @@ class Transaccion
         }
         return $transaccion;
     }
-    
-	public static function actualiza($usuario, $ID, $cantidad, $numero_tarjeta){
-        $ap = new Transaccion($ID, $usuario, $numero_tarjeta, $cantidad);
-        return $ap;
-    }
-	
-    public static function actualizaTransaccion($transaccion)
-	{
-		$result = false;
 
-		$app = Aplicacion::getSingleton();
-			$conn = $app->conexionBd();
-		  $query = sprintf("UPDATE transaccions SET  ID = '%s', ID_usuario = '%s', tarjeta = %d, cantidad = '%s'"
-		  , $transaccion->ID
-		  , $transaccion->ID_usuario
-		  , $transaccion->numero_tarjeta
-		  , $transaccion->cantidad);
-		$result = $conn->query($query);
-		if (!$result) {
-		  error_log($conn->error);  
-		} else if ($conn->affected_rows != 1) {
-		  error_log("Se han actualizado los datos '$conn->affected_rows' !");
-		}
-		return $result;
-	}    
-
-
-	private $ID_usuario;
 	private $ID;
+	private $ID_usuario;
 	private $cantidad;
 	private $numero_tarjeta;
+	private $ID_animal;
 
-	private function __construct($protectora, $usuario, $tarjeta, $cantidad)
+	private function __construct($id, $usuario, $cantidad, $tarjeta, $animal)
 	{
+		$this->ID = $id;
 		$this->ID_usuario = $usuario;
-		$this->ID = $protectora;
 		$this->cantidad = $cantidad;
 		$this->numero_tarjeta = $tarjeta;
+		$this->ID_animal = $animal;
 	}
 
 	/**
