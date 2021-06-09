7cee5d2915243128cd7d93511c4827f1a437764a
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2021-06-09 13:13:19 +0200

    Add files via upload

--- unipet_web/modificaProtectora.php
+++ unipet_web/modificaProtectora.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once __DIR__.'/includes/config.php';
-
-$form = new es\ucm\fdi\aw\FormularioEditaProtectora();
+if (isset($_GET['id'])){
+$form = new es\ucm\fdi\aw\FormularioEditaProtectora($_GET['id']);
 $htmlFormRegistro = $form->gestiona();
-
+}else $htmlFormRegistro = "<h1> No existe este animal </h1>";
 $tituloPagina = 'ActualizarProtect';
 
 $contenidoPrincipal = <<<EOS
